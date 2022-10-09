e7337a05b5f670c97b108b7917e493f38d056d54
usmansaeed2001 <muawan2001@gmail.com>
2022-10-09 18:59:37 +0500

    Grid Completed

--- script.js
+++ script.js
@@ -1,18 +1,28 @@
 
 const container = document.querySelector('#container');
-const num = parseInt(prompt('Please enter an even number: '));
+let num = 1;
+
+do {
+    num = parseInt(prompt('Please enter an number(1-64): '));
+} while (num < 1 || num > 64);
+
+
+let width = Math.floor(400 / num);
+
 
 let idv = 0;
 for (let i = 0; i < num; i++) {
-    const div = document.createElement('div');
-    div.classList.add('box');
-    div.setAttribute('id', idv);
-    container.appendChild(div);
-    idv++;
+    const upperdiv = document.createElement('div');
+    upperdiv.classList.add('row');
+    upperdiv.style.height = width + 'px';
+    for (let i = 0; i < num; i++) {
+        const div = document.createElement('div');
+        div.classList.add('box');
+        div.style.width = width + 'px';
+        div.style.height = width + 'px';
+        div.setAttribute('id', idv);
+        upperdiv.appendChild(div);
+        idv++;
+    }
+    container.appendChild(upperdiv);
 }
-
-
-document.getElementsByClassName('box').addEventListener("click", function() {
-    let elemetn = document.getElementsByClassName('box');
-    elemetn.classList.add('clicked');
-});
